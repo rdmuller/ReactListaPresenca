8e632995b70c8f69f851fbc7a4aac8e98532d2bd
Ronald Deived Muller <rdm.ronaldmuller@gmail.com>
2022-08-24 18:21:42 -0300

    estilizações e limpeza

---
+++ src/pages/home/index.jsx
@@ -0,0 +1,13 @@
+import './styles.css'
+
+function Home() {
+  return (
+    <div className="container">
+    <h1>Lista de presenças</h1>
+    <input type="text" placeholder="Digite o nome" />
+    <button type="button">Adicionar</button>
+    </div>
+  )
+}
+
+export default Home
