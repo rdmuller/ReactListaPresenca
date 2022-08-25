f254f2c22c7477f9cc6531b398d3898816f12838
Ronald Deived Muller <rdm.ronaldmuller@gmail.com>
2022-08-25 07:42:12 -0300

    iniciando com copoenentes

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,13 +1,20 @@
-import './styles.css'
+import React, {useState} from 'react';
+import './styles.css';
+import Card from '../../components/card';
 
 function Home() {
+  const [studentName, setStudentName] = useState(); // aqui posso definir um valor inicial useState('Ronald')
+
   return (
     <div className="container">
     <h1>Lista de presenças</h1>
-    <input type="text" placeholder="Digite o nome" />
+    <h1>Nome: {studentName}</h1>
+    <input type="text" placeholder="Digite o nome" onChange={e => setStudentName(e.target.value)}/>
     <button type="button">Adicionar</button>
+    <Card nome="Ronald" hora="07:42:01"/>
+    <Card nome="João" hora="17:29:59"/>
     </div>
   )
 }
 
-export default Home
+export default Home;
