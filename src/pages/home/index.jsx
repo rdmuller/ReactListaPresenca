bb7adada6d25989dbf922b173435d5e913da8fa3
Ronald Muller <ronald.muller@silsistemas.com.br>
2022-08-26 07:45:49 -0300

    usando o card dinamico

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -4,15 +4,27 @@ import Card from '../../components/card';
 
 function Home() {
   const [studentName, setStudentName] = useState(); // aqui posso definir um valor inicial useState('Ronald')
+  const [students, setStudents] = useState([]);
+
+  function addStudent() {
+    const newStudent = {
+      name: studentName,
+      time: new Date().toLocaleTimeString('pt-br', {hour: '2-digit', minute: '2-digit', second: '2-digit', })
+    }
+
+    setStudents(oldState => [...oldState, newStudent]);
+
+    console.log(students);
+  }
 
   return (
     <div className="container">
     <h1>Lista de presenças</h1>
-    <h1>Nome: {studentName}</h1>
     <input type="text" placeholder="Digite o nome" onChange={e => setStudentName(e.target.value)}/>
-    <button type="button">Adicionar</button>
-    <Card nome="Ronald" hora="07:42:01"/>
-    <Card nome="João" hora="17:29:59"/>
+    <button type="button" onClick={addStudent}>Adicionar</button>
+    {
+      students.map(student => <Card nome={student.name} hora={student.time} />)
+    }
     </div>
   )
 }
