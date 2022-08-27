1ae05e8bfbb4fb8993a275f108441bf5c6e35fa4
Ronald Deived Muller <rdm.ronaldmuller@gmail.com>
2022-08-27 09:09:17 -0300

    finalizando o exercicio

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,10 +1,11 @@
-import React, {useState} from 'react';
+import React, { useState, useEffect } from 'react';
 import './styles.css';
 import Card from '../../components/card';
 
 function Home() {
   const [studentName, setStudentName] = useState(); // aqui posso definir um valor inicial useState('Ronald')
   const [students, setStudents] = useState([]);
+  const [userGit, setUserGit] = useState({name: '', avatar: ''})
 
   function addStudent() {
     const newStudent = {
@@ -15,13 +16,46 @@ function Home() {
     setStudents(oldState => [...oldState, newStudent]);
   }
 
+  useEffect(() => {
+    fetch('https://api.github.com/users/rdmuller')
+    .then(response => response.json())
+    .then(data => {
+      setUserGit({
+        name: data.name,
+        avatar: data.avatar_url,
+      })
+    })
+
+    /* ### Outra forma de escrever o código acima
+    async function getUserGit() {
+      const response = await fetch('https://api.github.com/users/rdmuller');
+      const data = await response.json()
+      setUserGit({
+        name: data.name,
+        avatar: data.avatar_url,
+      })
+    }
+    getUserGit()*/
+
+  }, []);
+
   return (
     <div className="container">
-    <h1>Lista de presenças</h1>
+      <header>
+        <h1>Lista de presenças</h1>
+        <div>
+          <strong>{userGit.name}</strong>
+          <img src={userGit.avatar} alt="Foto do perfil" />
+        </div>
+      </header>
     <input type="text" placeholder="Digite o nome" onChange={e => setStudentName(e.target.value)}/>
     <button type="button" onClick={addStudent}>Adicionar</button>
     {
-      students.map(student => <Card nome={student.name} hora={student.time} />)
+      students.map(student => 
+        <Card 
+          key={student.time}
+          nome={student.name} 
+          hora={student.time} />)
     }
     </div>
   )
